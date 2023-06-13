773a0610bc8694f484581dde9cfb88931e1d83b8
Leonardo Dalla Porta <leonardodallaporta@gmail.com>
2023-06-13 12:15:28 +0200

    Add files via upload

---
+++ Channels_ECells.h
@@ -0,0 +1,280 @@
+#ifndef Channels_ECells_H
+#define Channels_ECells_H
+
+#include<cmath>
+#include "Variables.h"
+
+/***********************************************************/
+/*         FAST SODIUM CHANNEL FOR E-CELLS                 */
+/***********************************************************/
+double Na_steadystate_m_E(double memV){
+  double alpham=4.*0.1*(memV+33.)/(1.-exp(-0.1*(memV+33.)));;
+  double betam=4.*4.*exp(-0.083333333333*(memV+53.7));
+  return alpham/(alpham+betam);
+}
+double Na_inactgate_h_E(double variable,double memV){
+  double alphah=4.*0.07*exp(-0.1*(memV+50.));;
+  double betah=4.*1./(1.+exp(-0.1*(memV+20.)));
+  return (alphah*(1.0-variable))-(betah*variable);
+}
+/**/
+/***************************************************************/
+/*          DELAYED RECTIFIER - K CHANNEL- FOR E-CELLS         */
+/***************************************************************/
+double K_actgate_n_E(double variable, double memV){
+  double betan=4.*0.125*exp(-0.04*(memV+44.));
+  double alphan=4*0.01*(memV+34)/(1.-exp(-0.1*(memV+34.)));
+  return (alphan*(1.0-variable))-(betan*variable);
+}
+/**/
+/**********************************************************/
+/*                A-TYPE K CHANNEL FOR E-CELLS            */
+/**********************************************************/
+double A_steadystate_m_E(double memV){
+  return 1./(1.+exp(-(memV+50.)/20.));
+}
+double A_inactgate_h_E(double variable, double memV){
+  double inactivOpenRate=1./(1.+exp((memV+80.)/6.))/15.;
+  double inactivCloseRate=1./15.-1./(1.+exp((memV+80.)/6.))/15.;
+
+  return (inactivOpenRate+inactivCloseRate)*((inactivOpenRate/(inactivOpenRate+inactivCloseRate))-variable);
+}
+/**/
+/**********************************************************/
+/* SLOWLY INACTIVATING A-TYPE K CHANNEL FOR E-CELLS - KS  */
+/**********************************************************/
+double KS_actgate_m_E(double variable, double memV){
+  double minf=1./(1.+exp(-(memV+34.)/6.5));
+  double tau=8./(exp(-(memV+55.)/30.)+exp((memV+55.)/30.));
+  return (minf-variable)/tau;
+}
+/**/
+/**********************************************************/
+/*   HIGH THRESHOLD CALCIUM CHANNEL FOR E-CELLS  - Ca     */
+/**********************************************************/
+double Ca_act_m_E(double memV){
+  return 1./(1.+exp(-0.11111111*(memV+20.)));
+}
+/**/
+/**********************************************************/
+/*          ANOMALOUS RECTIFIER K CHANNEL FOR E-CELLS     */
+/**********************************************************/
+double AR_act_h_E(double memV){
+  return 1./(1.+exp((memV+75.0)*0.25));
+}
+/**/
+/**********************************************************/
+/*          PERSISTENT NA CHANNEL FOR E-CELLS - NaP       */
+/**********************************************************/
+double NaP_act_m_E(double memV){
+  return 1./(1.+exp(-(memV+55.7)/7.7));
+}
+/**/
+/**********************************************************/
+/*         NA DEPENDENT K  CHANNEL FOR E-CELLS * KNa       */
+/**********************************************************/
+double KNa_winf_E(double variable){
+  double fac=pow(38.7/variable,3.5);
+  return 0.37/(1.+fac);
+}
+/**/
+
+
+
+
+double mcurrentMcCormick(double variable, double memV){
+  double minf=1./(1.+exp(-0.1*(memV+35.)));
+  double taum=1000.0/(3.3*(exp(0.05*(memV+35.))+exp(-0.05*(memV+35.))));
+  return (minf-variable)/taum;
+}
+
+double mcurrentWang(double variable, double memV){
+  double minf=1./(1.+exp(-0.1666*(memV+44.)));
+  double taum=100.0/((exp(-0.083*(memV+44.))+exp(0.083*(memV+44.))));
+  return (minf-variable)/taum;
+}
+
+double mcurrentHay(double variable, double memV){
+  double alpham=0.0033*exp(0.1*(memV+35.));
+  double betam=0.0033*exp(-0.1*(memV+35.));
+  double minf=alpham/(alpham+betam);
+  double tadj=pow(2.3,(34.-21.)*0.1);
+  double taum=1./(tadj*(alpham+betam));
+
+  return (minf-variable)/taum;
+
+}
+
+/*H-Channels Affect Frequency, Power and Amplitude Fluctuations of Neuronal Network Oscillations*/
+double hchannel(double variable, double memV){
+  double linf=1./(1.+exp((memV+81.0)/7.0));
+  double taul=exp(0.033*(memV+75.0))/(0.02*(1.0+exp(0.083*(memV+75.0))));
+
+  return (linf-variable)/taul;
+}
+
+double hchannelHay(double variable, double memV){
+  double fact = 1./(exp((memV+154.9)/11.9)-1.);
+  double alpham=0.0063*(memV+154.9)*fact;
+  double betam=0.00193*exp(memV/33.1);
+  double minf=alpham/(alpham+betam);
+  double taum=1./(alpham+betam);
+
+  return (minf-variable)/taum;
+}
+
+
+double hchannelHill(double variable, double memV){
+  double mh=1./(1.+exp((memV+75.0)/5.5));
+  double aux_taum=exp(-14.59-0.086*memV) + exp(-1.87+0.0701*memV);
+  double taum=1./aux_taum;
+
+  return (mh-variable)/taum;
+}
+
+
+/**********************************************************/
+/*                The Dynamics start here                 */
+/**********************************************************/
+double syn_sigmoid(double memV){
+  return 1./(1.+exp(-0.5*(memV-20.)));
+}
+void allderivsE(double *y, double *dy, double IsynSoma, double IsynDend, int index, double gKNa_cond, double gKCa_cond,  double factorM, double factorH, int key_h){
+
+	const double phi_eq = Rpump*naEq*naEq*naEq/(naEq*naEq*naEq+kp3);
+
+  double NaChannel=0.0;
+	NaChannel=pow(Na_steadystate_m_E(y[0]),3)*y[1]*gNa_E*0.5*0.3*(y[0]-55.0);
+
+	double KChannel=0.0;
+	KChannel=y[2]*y[2]*y[2]*y[2]*gK_E*0.5*0.3*(y[0]+100.0);
+
+	double LeakChannelS=0.0;
+	LeakChannelS=glE[index]*0.3*(y[0]-VlE[index]);
+
+  double LeakChannelD=0.0;
+	LeakChannelD=glE[index]*0.7*(y[5]-VlE[index]);
+
+  double AChannel=0.0;
+  AChannel=A_steadystate_m_E(y[0])*A_steadystate_m_E(y[0])*A_steadystate_m_E(y[0]);
+  AChannel*=gA*0.5*0.3*y[3]*(y[0]+100.0);
+
+  double KSChannel=0.0;
+  KSChannel=gKS*0.5*0.3*y[4]*(y[0]+100.0);
+
+  /*Dendrite below - Soma above*/
+
+  double CaChannel=0.0;
+  CaChannel=gCa*0.5*0.7*Ca_act_m_E(y[5])*Ca_act_m_E(y[5])*(y[5]-120.0);
+
+  double NaPChannel=0.0;
+  NaPChannel=NaP_act_m_E(y[5])*NaP_act_m_E(y[5])*NaP_act_m_E(y[5]);
+  NaPChannel*=gNaP*0.5*0.7*(y[5]-55.0);
+
+  double ARChannel=0.0;
+  ARChannel=gAR*0.5*0.7*AR_act_h_E(y[5])*(y[5]+100.0);
+
+  double KCaChannel=0.0;
+  KCaChannel=gKCa_cond*0.5*0.7*y[6]/(y[6]+30.)*(y[5]+100.0);
+  KCaChannel=KCaChannel;
+
+  double KNaChannel=0.0;
+  KNaChannel=gKNa_cond*0.5*0.3*KNa_winf_E(y[7])*(y[0]+100.0);
+  KNaChannel=KNaChannel;
+
+  // if(keyKCa==1) cout << "WITH " << KCaChannel << endl;
+  // if(keyKCa==0) cout << "WITHOUT " << KCaChannel << endl;
+
+  double MChannel=0.0;
+  MChannel=gM*0.5*0.3*y[11]*(y[0]+100);
+
+  double Hchannel=0.0;
+  Hchannel=key_h*gH*0.5*0.7*y[12]*(y[5]+45.0);
+
+  const double iCsoma = 1./0.5/0.3;
+  const double iCdend = 1./0.5/0.7;
+  double iSoma = -LeakChannelS-NaChannel-KChannel-0.9*AChannel-0.7*KSChannel-0.56*KNaChannel-factorM*0.083*MChannel-gsd[index]*(y[0]-y[5])+IsynSoma;
+
+  double iDend = -LeakChannelD-CaChannel-1.*NaPChannel-1.*ARChannel-1.*KCaChannel-factorH*0.0115*Hchannel-gsd[index]*(y[5]-y[0])+IsynDend;
+
+/*    double iSoma = -LeakChannelS-NaChannel-KChannel-0.9*AChannel-0.7*KSChannel-0.56*KNaChannel-0.083*MChannel-gsd[index]*(y[0]-y[5])+IsynSoma;
+  double iDend = -LeakChannelD-CaChannel-1.*NaPChannel-1.*ARChannel-1.*KCaChannel-0.012*Hchannel-gsd[index]*(y[5]-y[0])+IsynDend;
+*/
+
+  dy[0] = iSoma*iCsoma;
+  dy[1] = Na_inactgate_h_E(y[1],y[0]);
+  dy[2] = K_actgate_n_E(y[2],y[0]);
+  dy[3] = A_inactgate_h_E(y[3],y[0]);
+  dy[4] = KS_actgate_m_E(y[4],y[0]);
+
+  dy[5] = iDend*iCdend;
+
+  dy[6] = -alphaCa*CaChannel-y[6]/tauCa;
+  double na3=y[7]*y[7]*y[7];
+  dy[7] = -naAlpha*(NaChannel+NaPChannel)-Rpump*na3/(na3+kp3) + phi_eq;
+
+  dy[8] = 3.48*syn_sigmoid(y[0]) - y[8]/2.0;
+  dy[9] = 0.5*y[10]*(1.-y[9])-y[9]/100.;
+  dy[10] = 3.48*syn_sigmoid(y[0]) - y[10]/2.;
+
+  dy[11] = mcurrentMcCormick(y[11],y[0]);
+  dy[12] = hchannelHill(y[12],y[5]);
+}
+
+/**/
+void rk4E(double curr, double cur, int ind, double gKNa_cond, double gKCa_cond,  double keyKNa, double keyKCa, double key_h){
+	int i;
+	double hh=dt*0.5;
+	double h6=dt*0.166666666666666667;
+	for (i=0;i<nVarsE;i++)
+		yt[i]=varsE[i]+hh*dVarsE[i];
+	allderivsE(yt,dyt,curr,cur,ind, gKNa_cond, gKCa_cond, keyKNa, keyKCa,key_h);
+	for (i=0;i<nVarsE;i++)
+		yt[i]=varsE[i]+hh*dyt[i];
+	allderivsE(yt,dym,curr,cur,ind, gKNa_cond, gKCa_cond, keyKNa, keyKCa,key_h);
+	for (i=0;i<nVarsE;i++) {
+		yt[i]=varsE[i]+dt*dym[i];
+		dym[i] += dyt[i];
+	}
+	allderivsE(yt,dyt,curr,cur,ind, gKNa_cond, gKCa_cond, keyKNa, keyKCa,key_h);
+	for (i=0;i<nVarsE;i++)
+		varsE[i] += h6*(dVarsE[i]+dyt[i]+2.0*dym[i]);
+}
+/**/
+void rungeKutta4E(double timesim, int index, double currExt1, double currExt2, double gKNa_cond, double gKCa_cond, double keyKNa, double keyKCa, double key_h){
+	varsE[0]=y[0];
+	varsE[1]=y[1];
+	varsE[2]=y[2];
+  varsE[3]=y[3];
+  varsE[4]=y[4];
+  varsE[5]=y[5];
+  varsE[6]=y[6];
+  varsE[7]=y[7];
+  varsE[8]=y[8];
+  varsE[9]=y[9];
+  varsE[10]=y[10];
+  varsE[11]=y[11];
+  varsE[12]=y[12];
+	allderivsE(varsE,dVarsE,currExt1,currExt2,index, gKNa_cond, gKCa_cond, keyKNa, keyKCa, key_h);
+  rk4E(currExt1,currExt2,index, gKNa_cond, gKCa_cond, keyKNa, keyKCa,  key_h);
+	y[0]=varsE[0];
+	y[1]=varsE[1];
+	y[2]=varsE[2];
+  y[3]=varsE[3];
+  y[4]=varsE[4];
+  y[5]=varsE[5];
+  y[6]=varsE[6];
+  y[7]=varsE[7];
+  y[8]=varsE[8];
+  y[9]=varsE[9];
+  y[10]=varsE[10];
+  y[11]=varsE[11];
+  y[12]=varsE[12];
+}
+/**/
+
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,284 @@
+#include<iostream>
+#include "Randomnumber.h"
+#include "Channels_ECells.h"
+#include "Channels_ICells.h"
+#include "Variables.h"
+#include "Network.h"
+#include "NeuronsStructure.h"
+
+using namespace std;
+
+
+
+
+int main (int argc, char *argv[]){
+
+
+
+  double transient_samples=10000.0; 
+  double factorM=0.;
+
+  /*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
+  /*&&*/if(argc != 3){
+	/*&&*/	cout << "#########-ERROR-########" << endl;
+	/*&&*/	cout << "# Give 2 paramenters as input:" << endl;
+	/*&&*/	cout << "# 1) Percentage factor for M current (betw 0-1) " << endl;
+  /*&&*/	cout << "# 2) seed " << endl;
+	/*&&*/	cout << "# Example: ./exct 0.1 1221314" << endl;
+	/*&&*/	cout << "########################" << endl;
+	/*&&*/	return 1;
+	/*&&*/}
+  factorM=(double) atof(argv[1]);
+  seed=(long) atoi(argv[2]);
+
+  /*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
+  const double KNa_percentage= 1.0; /*1st argument - Conductance for IKCa */
+  const double gKNa_conductance= 1.37;/*2nd argument - Conductance for IKNa */
+  const double KCa_percentage= 1.0; /*3rd argument - Conductance for IKCa */
+  const double gKCa_conductance= 0.57;/*4th argument - Conductance for IKNa */
+  const int keychannel=1;
+  long seed1=seed;
+  /*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
+
+
+  startpointers();
+
+
+  /*Only 30% of py neurons have the H channel*/
+  int *neuronsH=new int[300];
+  for(int i=0;i<300;i++){
+    int auxkey=0;
+  	if(i==0) neuronsH[i]=int(ran(&seed)*1024);
+    if(i!=0){
+      do{
+        neuronsH[i]=int(ran(&seed)*1024);
+        for(int k=0;k<i;k++){
+          if(neuronsH[i]==neuronsH[k]){
+            auxkey=1;
+            break;
+          }
+          else auxkey=0;
+        }
+      }
+      while(auxkey==1);
+    }
+  }
+  /**/
+
+
+	construct_network();
+
+  int *electrode=new int [2];
+
+  electrode[0]=400+int(ran(&seed)*400);
+  electrode[1]=600+int(ran(&seed)*400);
+
+  int countLPF=0;
+  int countSingleNeuron=0;
+  int auxindex=0;
+
+
+  FILE *outf, *outf2, *outf3, *outf4,*outf5;
+	char *Outfile;
+  char *Outfile2;
+  char *Outfile3;
+  char *Outfile4;
+  char *Outfile5;
+
+	Outfile=new char[500];
+  Outfile2=new char[500];
+  Outfile3=new char[500];
+  Outfile4=new char[500];
+  Outfile5=new char[500];
+
+  sprintf(Outfile,"Raster_factorM_%.3lf_seed%ld.ts",factorM,seed1);
+  sprintf(Outfile2,"LFP_factorM_%.3lf_seed%ld.ts",factorM,seed1);
+  sprintf(Outfile3,"NEURONS_factorM_%.3lf_seed%ld.ts",factorM,seed1);
+  sprintf(Outfile4,"FR_GLOBAL_factorM_%.3lf_seed%ld.ts",factorM,seed1);
+  sprintf(Outfile5,"Synchrony__factorM_%.3lf_seed%ld.ts",factorM,seed1);
+
+
+  outf = fopen(Outfile,"w");
+  outf2 = fopen(Outfile2,"w");
+  outf3 = fopen(Outfile3,"w");
+
+
+
+  const int number_sync_cells=512;
+
+  double **monitoring_cell;
+  double meanV=0.0;
+  double meanV_sqr=0.0;
+  double aux_meanV=0.0;
+  monitoring_cell = new double*[number_sync_cells];
+  for(int i=0;i<number_sync_cells;i++) monitoring_cell[i]= new double[2];
+
+
+  for(int t=1;t<timesimulation;t++){
+    double auxsumLFP=0.0;
+    int aux_FR=0;
+    double currtimesimu=t*dt*0.001; /*current time simulation*/
+
+    ComputeSynpases();
+
+    /*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+    /*Excitatory looping over time - EXC DYNAMICS*/
+    /**/for(int j=0;j<numEcells;j++){
+    /**/  double eCondAmpa=gEE_A*EESynInputAMPA[j];
+    /**/  double eCondNMDA=gEE_N*EESynInputNMDA[j]/(1.0 + 0.2801*exp(-0.062*memV_DendE[j]));
+    /**/  double iCondGABA=gIE*IESynInput[j];
+    /**/  double currtodend=(eCondAmpa+eCondNMDA)*(vSynGlu-memV_DendE[j]);
+    /**/  double currtosoma=iCondGABA*(vSynGABA-memV_SomaE[j]);
+    /**/
+    /**/  y[0]=memV_SomaE[j];y[1]=h_Na_E[j];
+    /**/  y[2]=n_K_E[j];y[3]=h_A_E[j];
+    /**/  y[4]=m_KS_E[j];y[5]=memV_DendE[j];
+    /**/  y[6]=conc_Ca_E[j];y[7]=conc_Na_E[j];
+    /**/  y[8]= sAmpaE[j];y[9]= sNmdaE[j];
+    /**/  y[10]= xNmdaE[j]; y[11]=mcurrent[j]; y[12]=hcurrent[j];
+    /**/  for(int z=0;z<nVarsE;z++){
+    /**/    dVarsE[z]=aux_dvarsE[j][z];
+    /**/    dym[z]=aux_dym[j][z];
+    /**/    dyt[z]=aux_dyt[j][z];
+    /**/    yt[z]=aux_yt[j][z];
+    /**/  }
+    /**/
+          int key_h=0;
+          for(int ii=0;ii<300;ii++){
+            if(j==neuronsH[ii]) key_h=1;
+          }
+    /**/  rungeKutta4E(currtimesimu,j,currtosoma,currtodend, gKNa_conductance, gKCa_conductance, factorM, 1.0, key_h);
+
+          /*Implementing the local synchrony*/
+
+          if(t>transient_samples){
+            if(j>=256 and j<768){
+              for(int ii=0;ii<number_sync_cells;ii++){
+                if((ii+256)==j){
+                  monitoring_cell[j-256][0]+=y[0]*y[0];
+                  monitoring_cell[j-256][1]+=y[0]/(timesimulation-transient_samples-1.0);
+                  aux_meanV+=y[0]*(1./number_sync_cells);
+                  break;
+                }
+              }
+            }
+          }
+
+          if(t>transient_samples){
+            if(j<=electrode[0]+25 and j>=electrode[0]-25) auxsumLFP+=abs(currtosoma)+abs(currtodend);
+          }
+
+          if(t>transient_samples){
+
+            if(((memV_SomaE[j]-vth)*(y[0]-vth)<0.0) && (y[0]>vth)){
+              fprintf(outf,"%lf\t%d\t1\n", (t-transient_samples)*dt*0.001, j);
+            }
+            if(j==500)   fprintf(outf3,"%lf\t%lf\t", (t-transient_samples)*dt*0.001, y[0]);
+            if(j==501)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==502)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==503)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==504)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==505)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==506)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==507)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==508)   fprintf(outf3,"%lf\t", y[0]);
+            if(j==509)   fprintf(outf3,"%lf\n", y[0]);
+          }
+
+    /**/
+    /**/  memV_SomaE[j]=y[0];h_Na_E[j]=y[1];
+    /**/  n_K_E[j]=y[2];h_A_E[j]=y[3];
+    /**/  m_KS_E[j]=y[4];memV_DendE[j]=y[5];
+    /**/  conc_Ca_E[j]=y[6];conc_Na_E[j]=y[7];
+    /**/  sAmpaE[j]=y[8];sNmdaE[j]=y[9];
+    /**/  xNmdaE[j]=y[10]; mcurrent[j]=y[11]; hcurrent[j]=y[12];
+    /**/  for(int z=0;z<nVarsE;z++){
+    /**/    aux_dvarsE[j][z]=dVarsE[z];
+    /**/    aux_dym[j][z]=dym[z];
+    /**/    aux_dyt[j][z]=dyt[z];
+    /**/    aux_yt[j][z]=yt[z];
+    /**/  }
+    /**/}/*End looping EXC*/
+    if(t>transient_samples){
+      meanV+=(aux_meanV)/(timesimulation-transient_samples-1.0);
+      meanV_sqr+=aux_meanV*aux_meanV;
+      aux_meanV=0.0;
+    }
+
+    /**//*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+    if(t>transient_samples){
+      fprintf(outf2,"%lf\t%lf\n", (t-transient_samples)*dt*0.001, auxsumLFP);
+    }
+    /*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+    /*Inhibitory looping over time - EXC DYNAMICS*/
+    /**/for(int j=0;j<numIcells;j++){
+    /**/  double eCondAmpa=gEI_A*EISynInputAMPA[j];
+    /**/  double eCondNMDA=gEI_N*EISynInputNMDA[j]/(1.0 + 0.2801*exp(-0.062*memV_SomaI[j]));
+    /**/  double iCondGABA=gII*IISynInput[j];
+    /**/  double currtodend=(eCondAmpa+eCondNMDA)*(vSynGlu-memV_SomaI[j]);
+    /**/  double currtosoma=iCondGABA*(vSynGABA-memV_SomaI[j]);
+    /**/
+    /**/  x[0]=memV_SomaI[j];
+    /**/  x[1]=h_Na_I[j];
+    /**/  x[2]=n_K_I[j];
+    /**/  x[3]=sGabaI[j];
+    /**/  for(int z=0;z<nVarsI;z++){
+    /**/    aux_dvarsI[j][z]=dVarsI[z];
+    /**/    aux_dxm[j][z]=dxm[z];
+    /**/    aux_dxt[j][z]=dxt[z];
+    /**/    aux_xt[j][z]=xt[z];
+    /**/  }
+    /**/
+    /**/  rungeKutta4I(currtimesimu,j, currtodend+currtosoma);
+          if(t>transient_samples){
+            if(((memV_SomaI[j]-vth)*(x[0]-vth)<0.0) && (x[0]>vth))   fprintf(outf,"%lf\t%d\t0\n", (t-transient_samples)*dt*0.001, j);
+          }
+    /**/  //if(((memV_SomaI[j]-vth)*(x[0]-vth)<0.0))  cout << t*dt*0.001 << "\t" << j << "\t" << 0 << endl;
+    /**/
+    /**/  memV_SomaI[j]=x[0];h_Na_I[j]=x[1];
+    /**/  n_K_I[j]=x[2];sGabaI[j]=x[3];
+    /**/  for(int z=0;z<nVarsI;z++){
+    /**/    aux_dvarsI[j][z]=dVarsI[z];
+    /**/    aux_dxm[j][z]=dxm[z];
+    /**/    aux_dxt[j][z]=dxt[z];
+    /**/    aux_xt[j][z]=xt[z];
+    /**/  }
+    /**/}/*End Looping inh*/
+    /*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+
+	}/*END LOOP TIME*/
+
+  fclose(outf);
+  fclose(outf2);
+  fclose(outf3);
+
+  // fclose(outf4);
+
+
+
+  /*Synchorny Parameter*/
+  double variance_glogal = (meanV_sqr/(timesimulation-transient_samples-1.0))-(meanV*meanV);
+
+  double variance_local=0.0;
+  for(int i=0;i<number_sync_cells;i++){
+    variance_local+=((monitoring_cell[i][0]/(timesimulation-transient_samples-1.0))-(monitoring_cell[i][1]*monitoring_cell[i][1]))/number_sync_cells;
+  }
+
+  double synchrony=sqrt(variance_glogal/(variance_local));
+  outf5 = fopen(Outfile5,"w");
+  fprintf(outf5,"#Global_variance Local_variance Synchronization Parameter\n");
+  fprintf(outf5,"%lf\t%lf\t%lf\n",variance_glogal,variance_local,synchrony);
+  fclose(outf5);
+
+
+  for(int i=0;i<number_sync_cells;i++) monitoring_cell[i];
+  delete [] monitoring_cell;
+
+
+
+  deletepointersMonitoringChannels();
+  deletepointers();
+  delete [] electrode;
+
+	return 0;
+}
